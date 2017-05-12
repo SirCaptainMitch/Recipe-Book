a1d719d31662f572489d6f2cbee419eb46dde6cf
Mitchell Hamann <captain@voyager.local>
2017-05-12 18:09:04 -0500

    adjusted links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,14 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
-    { path: '' , redirectTo:  '/recipes' },
+    { path: '' , redirectTo:  '/recipes', pathMatch: 'full' },
     { path: 'recipes' , component: RecipesComponent },
     { path: 'shoppinglist', component: ShoppingListComponent }
 ]
 
 
 @NgModule({
-    imports: [RouterModule.forChild(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule { 
